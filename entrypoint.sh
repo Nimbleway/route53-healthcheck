6d9c5206e06de324819d5109adae5ee8fff56126
Eyal Halfon <eyalh@nimbleway.com>
2022-12-12 10:48:52 +0200

    33

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 # Used in ci/cd pipeline.
 # This script will upsert route53 healthcheck's to support multivalue dns.
-
+ls
 DOMAIN=`yq '.spec.tls[0].hosts[0]' ${CONFIG_FILE} | grep -v null | grep -v '\-' | head -n 1`
 echo "DOMAIN $DOMAIN"
 DOMAIN_ESCAPED=`echo $DOMAIN | sed 's/\./-/g'`
